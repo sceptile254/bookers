21486d24e70d7bc8f5cca0b081c9daa034102025
EC2 Default User <ec2-user@ip-172-31-23-88.ec2.internal>
2022-03-12 10:57:49 +0000

    レイアウト途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,35 @@
 class BooksController < ApplicationController
+
   def new
+
   end
 
   def index
+    #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @book = Book.new
+    #すべてのデータを取得し、@booksに格納
+    @books = Book.all
   end
+
+  def create #createアクションを定義
+    #データを受け取り新規登録するためのインスタンス
+    book = Book.new(book_params)
+    #データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    #トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
 
   def show
   end
 
   def edit
+  end
+
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/books' => "books#index"
+  post 'books' => 'books#create'
   get 'books/new'
   get 'books/index'
   get 'books/show'
