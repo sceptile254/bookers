453e9f3f6f225ef76bc3d09dbdfad5e9a439ab78
EC2 Default User <ec2-user@ip-172-31-23-88.ec2.internal>
2022-03-20 11:47:27 +0000

    編集画面を整えよう

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,10 +13,17 @@ class BooksController < ApplicationController
   def create #createアクションを定義
     #データを受け取り新規登録するためのインスタンス
     book = Book.new(book_params)
+
+    if book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(book.id)
+    else
+      redirect_to '/books'
+    end
     #データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    #トップ画面へリダイレクト
-    redirect_to '/'
+    #book.save
+    #詳細画面へリダイレクト
+    #redirect_to book_path(book.id)
   end
 
 
@@ -25,6 +32,8 @@ class BooksController < ApplicationController
   end
 
   def edit
+    # 保存済みのデータが必要なので、findでデータを取得し、@bookに収納
+    @book = Book.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get '/' => "homes#top"
   get 'books' => "books#index"
   post 'books' => 'books#create'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   resources :books
 end
