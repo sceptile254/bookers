5c0811a41ca069942d9fb5e633463060daac01b5
EC2 Default User <ec2-user@ip-172-31-23-88.ec2.internal>
2022-03-20 13:54:44 +0000

    バリデーションの実装から

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,6 +36,30 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book.id)
+    else
+      redirect_to '/books'
+    end
+
+  end
+
+  def destroy
+    book = Book.find(params[:id]) #レコードを1件取得し、bookに格納
+
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to '/books'
+    else
+      flash[:notice] = "Book was NOT successfully destroyed."
+      redirect_to '/books'
+    end
+  end
+
   private
   #ストロングパラメータ
   def book_params
